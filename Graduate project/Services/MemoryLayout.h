de7e5a3c93be912af93ccc7930addd247982cd64
Mostafa ElFallal <mostafaelfallal90@gmail.com>
2022-05-18 20:54:42 +0200

    Automated

--- Graduate project/Services/MemoryLayout.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MEMLAYOUT__H_
-#define __MEMLAYOUT__H_
-#include "main.h"
-
-void FrameStore(Frame_t * frame);    // store this frame in MRAM
-void FrameLoad(uint16_t numOfFrames);   // Load the last numOfFrames in transmitter task
-#endif
